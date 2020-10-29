94ba9bb012dc32e46dbdd8038afbc9afd3c675b6
DLBY <david.lobry96@gmail.com>
2020-10-29 21:43:43 +0100

    you can logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+=begin
+  include sessions_helper
+
+    if current_user
+    end
+=end
 end
+
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    #    The ||= part ensures this helper doesn't hit the database every time a user hits a web page. It will look it up once, then cache it in the @current_user variable.
+
+    #This is called memoization and it helps make our app more efficient and scalable.
+
+  end
 end
